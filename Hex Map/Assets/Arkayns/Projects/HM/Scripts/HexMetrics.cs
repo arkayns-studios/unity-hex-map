182c9731926ad8d6d6b1f0197ed495926b27efac
Serge Maia <sergemaia9@gmail.com>
2022-01-26 09:51:05 +0000

    REF: Changed name convention

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMetrics.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMetrics.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 namespace Arkayns.HM {
     
     public static class HexMetrics {
-        public const float outerRadius = 10f;
-        public const float innerRadius = outerRadius * 0.866025404f; // √e2−(e2)2 = √3e24 = e√32 ≈ 0.886e
+        public const float OuterRadius = 10f;
+        public const float InnerRadius = OuterRadius * 0.866025404f; // √e2−(e2)2 = √3e24 = e√32 ≈ 0.886e
         
-        public static Vector3[] corners = {
-            new Vector3(0f, 0f, outerRadius), new Vector3(innerRadius, 0f, 0.5f * outerRadius), new Vector3(innerRadius, 0f, -0.5f * outerRadius),
-            new Vector3(0f, 0f, -outerRadius), new Vector3(-innerRadius, 0f, -0.5f * outerRadius), new Vector3(-innerRadius, 0f, 0.5f * outerRadius)
+        public static Vector3[] Corners = {
+            new Vector3(0f, 0f, OuterRadius), new Vector3(InnerRadius, 0f, 0.5f * OuterRadius), new Vector3(InnerRadius, 0f, -0.5f * OuterRadius),
+            new Vector3(0f, 0f, -OuterRadius), new Vector3(-InnerRadius, 0f, -0.5f * OuterRadius), new Vector3(-InnerRadius, 0f, 0.5f * OuterRadius)
         };
         
     } // Class HexMetrics
