69dd2dd3dafb0c5be4ce3575cc8f3f5af63d2e93
Serge Maia <sergemaia9@gmail.com>
2022-01-26 09:47:20 +0000

    ADD: Showing Coordinates on the Grid

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexGrid.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexGrid.cs
@@ -1,5 +1,5 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Arkayns.HM {
     
@@ -10,7 +10,11 @@ namespace Arkayns.HM {
         public HexCell cellPrefab;
         private HexCell[] m_cells;
 
+        public Text cellLabelPrefab;
+        private Canvas m_gridCanvas;
+        
         private void Awake() {
+            m_gridCanvas = GetComponentInChildren<Canvas>();
             m_cells = new HexCell[height * width];
 
             for (int z = 0, i = 0; z < height; z++) {
@@ -25,9 +29,12 @@ namespace Arkayns.HM {
             position.y = 0f;
             position.z = z * 10f;
 
-            HexCell cell = m_cells[i] = Instantiate<HexCell>(cellPrefab);
-            cell.transform.SetParent(transform, false);
+            HexCell cell = m_cells[i] = Instantiate<HexCell>(cellPrefab, transform, false);
             cell.transform.localPosition = position;
+
+            Text label = Instantiate<Text>(cellLabelPrefab, m_gridCanvas.transform, false);
+            label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+            label.text = $"{x}\n{z}";
         } // CreateCell
 
     } // Class HexGrid
