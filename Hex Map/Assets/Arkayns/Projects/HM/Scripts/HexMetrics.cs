efaa49a50dfb23b86e6ca35d71204f21b9d54e61
Serge Maia <sergemaia9@gmail.com>
2022-01-26 10:25:59 +0000

    RED: Adding a seventh corner pointing to the first one

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMetrics.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMetrics.cs
@@ -6,9 +6,14 @@ namespace Arkayns.HM {
         public const float OuterRadius = 10f;
         public const float InnerRadius = OuterRadius * 0.866025404f; // √e2−(e2)2 = √3e24 = e√32 ≈ 0.886e
         
-        public static Vector3[] Corners = {
-            new Vector3(0f, 0f, OuterRadius), new Vector3(InnerRadius, 0f, 0.5f * OuterRadius), new Vector3(InnerRadius, 0f, -0.5f * OuterRadius),
-            new Vector3(0f, 0f, -OuterRadius), new Vector3(-InnerRadius, 0f, -0.5f * OuterRadius), new Vector3(-InnerRadius, 0f, 0.5f * OuterRadius)
+        public static readonly Vector3[] Corners = {
+            new Vector3(0f, 0f, OuterRadius), 
+            new Vector3(InnerRadius, 0f, 0.5f * OuterRadius), 
+            new Vector3(InnerRadius, 0f, -0.5f * OuterRadius),
+            new Vector3(0f, 0f, -OuterRadius), 
+            new Vector3(-InnerRadius, 0f, -0.5f * OuterRadius), 
+            new Vector3(-InnerRadius, 0f, 0.5f * OuterRadius),
+            new Vector3(0f, 0f, OuterRadius)
         };
         
     } // Class HexMetrics
