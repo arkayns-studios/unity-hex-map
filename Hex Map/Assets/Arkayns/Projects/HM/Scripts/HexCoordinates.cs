1e77d598aa6750ebbd7927a7c6e31352ada2fdec
Serge Maia <sergemaia9@gmail.com>
2022-01-26 11:23:31 +0000

    ADD: Coordinates in the Inspector to HexCells

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexCoordinates.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexCoordinates.cs
@@ -1,28 +1,48 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Arkayns.HM {
-    
+
     [System.Serializable]
     public struct HexCoordinates {
-        public  int X { get; private set; }
-        public  int Z { get; private set; }
+        [SerializeField] private int m_x, m_z;
+
+        public int X => m_x;
         public int Y => -X - Z;
-        
+        public int Z => m_z;
+
         public HexCoordinates(int x, int z) {
-            X = x;
-            Z = z;
+            m_x = x;
+            m_z = z;
         } // Constructor HexCoordinates
-        
-        public static HexCoordinates FromOffsetCoordinates (int x, int z) {
-            return new HexCoordinates(x -z / 2, z);
+
+        public static HexCoordinates FromOffsetCoordinates(int x, int z) {
+            return new HexCoordinates(x - z / 2, z);
         } // FromOffsetCoordinates
-        
-        public string ToStringOnSeparateLines () {
+
+        public string ToStringOnSeparateLines() {
             return $"{X}\n{Y}\n{Z}";
         } // ToStringOnSeparateLines
-        
-        public override string ToString () {
-            return $"({X}, {Y}, {Z})";
+
+        public override string ToString() {
+            return $"[{X}, {Y}, {Z}]";
         } // Override ToString
-        
+
     } // Struct HexCoordinates
-    
+
+#if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(HexCoordinates))]
+    public class HexCoordinatesDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            HexCoordinates coordinates = new HexCoordinates(property.FindPropertyRelative("m_x").intValue, property.FindPropertyRelative("m_z").intValue);
+            GUI.Label(position, label);
+            position = EditorGUI.PrefixLabel(position, label);
+            GUI.Label(position, coordinates.ToString());
+        } // Override OnGUI
+        
+    } // HexCoordinatesDrawer
+#endif
+
 } // Namespace Arkayns HM
