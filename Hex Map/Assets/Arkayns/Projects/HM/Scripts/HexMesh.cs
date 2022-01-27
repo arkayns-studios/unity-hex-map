69ad4ed61dd9c3c630bf9d4815523be0d8f29beb
Serge Maia <sergemaia9@gmail.com>
2022-01-27 11:40:57 +0000

    FIX: Triangular Connections, plug the triangular holes again.

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMesh.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMesh.cs
@@ -70,6 +70,12 @@ namespace Arkayns.HM {
             Vector3 bridge = HexMetrics.GetBridge(direction);
             Vector3 v3 = v1 + bridge;
             Vector3 v4 = v2 + bridge;
+            
+            HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
+            if (direction <= HexDirection.E && nextNeighbor != null) {
+                AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
+                AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
+            }
 
             AddQuad(v1, v2, v3, v4);
             AddQuadColor(cell.color, neighbor.color);
