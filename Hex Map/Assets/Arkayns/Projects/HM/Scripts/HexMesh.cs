c8f3fbc89b2f22a256f50acb1179f74dd5160ef9
Serge Maia <sergemaia9@gmail.com>
2022-07-20 10:43:39 +0100

    ADD:Double Cliffs
    The only remaining non-flat cases are those where the bottom cell has cliffs on both sides.
    This leaves all options open for the top edge.
    It could be either flat, a slope, or a cliff.
    We're only interested in the cliff-cliff-slope case, as it is the only one with terraces.

--- Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMesh.cs
+++ Hex Map/Assets/Arkayns/Projects/HM/Scripts/HexMesh.cs
@@ -180,6 +180,12 @@ namespace Arkayns.HM {
                 return;
             }
             
+            if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
+                if (leftCell.Elevation < rightCell.Elevation) TriangulateCornerCliffTerraces(right, rightCell, bottom, bottomCell, left, leftCell);
+                else TriangulateCornerTerracesCliff(left, leftCell, right, rightCell, bottom, bottomCell);
+                return;
+            }
+            
             AddTriangle(bottom, left, right);
             AddTriangleColor(bottomCell.color, leftCell.color, rightCell.color);
         } // TriangulateCorner
@@ -212,6 +218,7 @@ namespace Arkayns.HM {
         
         private void TriangulateCornerTerracesCliff (Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell) {
             float b = 1f / (rightCell.Elevation - beginCell.Elevation);
+            if (b < 0) b = -b;
             Vector3 boundary = Vector3.Lerp(begin, right, b);
             Color boundaryColor = Color.Lerp(beginCell.color, rightCell.color, b);
             
@@ -219,8 +226,7 @@ namespace Arkayns.HM {
             
             if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
                 TriangulateBoundaryTriangle(left, leftCell, right, rightCell, boundary, boundaryColor);
-            }
-            else {
+            } else {
                 AddTriangle(left, right, boundary);
                 AddTriangleColor(leftCell.color, rightCell.color, boundaryColor);
             }
@@ -228,6 +234,7 @@ namespace Arkayns.HM {
         
         private void TriangulateCornerCliffTerraces (Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell) {
             float b = 1f / (leftCell.Elevation - beginCell.Elevation);
+            if (b < 0) b = -b;
             Vector3 boundary = Vector3.Lerp(begin, left, b);
             Color boundaryColor = Color.Lerp(beginCell.color, leftCell.color, b);
             
@@ -235,8 +242,7 @@ namespace Arkayns.HM {
             
             if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
                 TriangulateBoundaryTriangle(left, leftCell, right, rightCell, boundary, boundaryColor);
-            }
-            else {
+            } else {
                 AddTriangle(left, right, boundary);
                 AddTriangleColor(leftCell.color, rightCell.color, boundaryColor);
             }
