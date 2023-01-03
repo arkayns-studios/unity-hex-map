6e626c2067287e375476f42386e500cbfed488b7
Serge Maia <sergemaia9@gmail.com>
2023-01-03 09:36:26 +0000

    ADD: Optional Color and Elevation
    We can make colors optional by adding a blank option to the toggle group.

--- Hex Map/Assets/Arkayns/Reckon/HM/Utilities/HexMapEditor.cs
+++ Hex Map/Assets/Arkayns/Reckon/HM/Utilities/HexMapEditor.cs
@@ -9,36 +9,45 @@ namespace Arkayns.Reckon.HM {
 		public HexGrid hexGrid;
 		private int m_activeElevation;
 		private Color m_activeColor;
+		private bool applyColor;
+		private bool applyElevation;
 
-		// -- Methods --
+		// -- Built-In Methods --
 		private void Awake () {
-			SelectColor(0);
-		} // Awake
+			SelectColor(-1);
+			SetApplyElevation(true);
+		} // Awake ()
 
 		private void Update () {
-			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
-				HandleInput();
-			}
-		} // Update
-		
-		public void SelectColor (int index) => m_activeColor = colors[index];
+			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) HandleInput();
+		} // Update ()
 
-		public void SetElevation (float elevation) => m_activeElevation = (int)elevation;
-		
-		
 		// -- Methods --
+		public void SelectColor(int index) {
+			applyColor = index >= 0;
+			if (applyColor) m_activeColor = colors[index];
+		} // SelectColor ()
+		
+		public void SetApplyElevation(bool toggle) {
+			applyElevation = toggle;
+		} // SetApplyElevation ()
+
+		public void SetElevation(float elevation) {
+			if (applyElevation) m_activeElevation = (int)elevation;
+		} // SetElevation ()
+		
 		private void HandleInput () {
 			Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			if (Physics.Raycast(inputRay, out hit)) {
 				EditCell(hexGrid.GetCell(hit.point));
 			}
-		} // HandleInput
+		} // HandleInput ()
 
 		private void EditCell (HexCell cell) {
-			cell.Color = m_activeColor;
-			cell.Elevation = m_activeElevation;
-		} // EditCell
+			if (applyColor) cell.Color = m_activeColor;
+			if (applyElevation) cell.Elevation = m_activeElevation;
+		} // EditCell ()
 		
 	} // Class HexMapEditor
 	
