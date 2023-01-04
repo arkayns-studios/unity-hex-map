fda9bd2e13193fb5d72f4ac6d11f1385d67d767f
Serge Maia <sergemaia9@gmail.com>
2023-01-04 13:41:52 +0000

    ADD: Matching the Channel

--- Hex Map/Assets/Arkayns/Reckon/HM/Internal/HexMesh.cs
+++ Hex Map/Assets/Arkayns/Reckon/HM/Internal/HexMesh.cs
@@ -98,6 +98,14 @@ namespace Arkayns.Reckon.HM {
         } // TriangulateWithRiverBeginOrEnd ()
 
         private void TriangulateAdjacentToRiver(HexDirection direction, HexCell cell, Vector3 center, EdgeVertices e) {
+            if (cell.HasRiverThroughEdge(direction.Next())) {
+                if (cell.HasRiverThroughEdge(direction.Previous())) 
+                    center += HexMetrics.GetSolidEdgeMiddle(direction) * (HexMetrics.innerToOuter * 0.5f);
+                else if (cell.HasRiverThroughEdge(direction.Previous2()))
+                    center += HexMetrics.GetFirstSolidCorner(direction) * 0.25f;
+            } else if (cell.HasRiverThroughEdge(direction.Previous()) && cell.HasRiverThroughEdge(direction.Next2()))
+                center += HexMetrics.GetSecondSolidCorner(direction) * 0.25f;
+            
             var m = new EdgeVertices(Vector3.Lerp(center, e.v1, 0.5f), Vector3.Lerp(center, e.v5, 0.5f));
             TriangulateEdgeStrip(m, cell.Color,e, cell.Color);
             TriangulateEdgeFan(center, m, cell.Color);
