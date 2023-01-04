6f1ee1dbda41a1321dd3aa8e38179f7870ecb850
Serge Maia <sergemaia9@gmail.com>
2023-01-04 12:02:33 +0000

    ADD: Adding an Edge Vertex

--- Hex Map/Assets/Arkayns/Reckon/HM/Internal/HexMesh.cs
+++ Hex Map/Assets/Arkayns/Reckon/HM/Internal/HexMesh.cs
@@ -13,16 +13,14 @@ namespace Arkayns.Reckon.HM {
 
         private Mesh m_hexMesh;
         private MeshCollider m_meshCollider;
-
-
+        
         // -- Built-In Methods --
         private void Awake() {
             GetComponent<MeshFilter>().mesh = m_hexMesh = new Mesh();
             m_meshCollider = gameObject.AddComponent<MeshCollider>();
             m_hexMesh.name = "Hex Mesh";
         } // Awake
-
-
+        
         // -- Methods --
         public void Triangulate(HexCell[] cells) {
             m_hexMesh.Clear();
@@ -43,8 +41,7 @@ namespace Arkayns.Reckon.HM {
 
         private void Triangulate(HexDirection direction, HexCell cell) {
             var center = cell.Position;
-            var e = new EdgeVertices(center + HexMetrics.GetFirstSolidCorner(direction),
-                center + HexMetrics.GetSecondSolidCorner(direction));
+            var e = new EdgeVertices(center + HexMetrics.GetFirstSolidCorner(direction), center + HexMetrics.GetSecondSolidCorner(direction));
 
             TriangulateEdgeFan(center, e, cell.Color);
             if (direction <= HexDirection.SE) TriangulateConnection(direction, cell, e);
@@ -56,7 +53,7 @@ namespace Arkayns.Reckon.HM {
 
             var bridge = HexMetrics.GetBridge(direction);
             bridge.y = neighbor.Position.y - cell.Position.y;
-            var e2 = new EdgeVertices(e1.v1 + bridge, e1.v4 + bridge);
+            var e2 = new EdgeVertices(e1.v1 + bridge, e1.v5 + bridge);
 
             if (cell.GetEdgeType(direction) == HexEdgeType.Slope) TriangulateEdgeTerraces(e1, cell, e2, neighbor);
             else TriangulateEdgeStrip(e1, cell.Color, e2, neighbor.Color);
@@ -64,22 +61,19 @@ namespace Arkayns.Reckon.HM {
             var nextNeighbor = cell.GetNeighbor(direction.Next());
             if (direction > HexDirection.E || nextNeighbor == null) return;
 
-            var v5 = e1.v4 + HexMetrics.GetBridge(direction.Next());
+            var v5 = e1.v5 + HexMetrics.GetBridge(direction.Next());
             v5.y = nextNeighbor.Position.y;
 
             if (cell.Elevation <= neighbor.Elevation) {
                 if (cell.Elevation <= nextNeighbor.Elevation) {
-                    TriangulateCorner(e1.v4, cell, e2.v4, neighbor, v5, nextNeighbor);
+                    TriangulateCorner(e1.v5, cell, e2.v5, neighbor, v5, nextNeighbor);
+                } else {
+                    TriangulateCorner(v5, nextNeighbor, e1.v5, cell, e2.v5, neighbor);
                 }
-                else {
-                    TriangulateCorner(v5, nextNeighbor, e1.v4, cell, e2.v4, neighbor);
-                }
-            }
-            else if (neighbor.Elevation <= nextNeighbor.Elevation) {
-                TriangulateCorner(e2.v4, neighbor, v5, nextNeighbor, e1.v4, cell);
-            }
-            else {
-                TriangulateCorner(v5, nextNeighbor, e1.v4, cell, e2.v4, neighbor);
+            } else if (neighbor.Elevation <= nextNeighbor.Elevation) {
+                TriangulateCorner(e2.v5, neighbor, v5, nextNeighbor, e1.v5, cell);
+            } else {
+                TriangulateCorner(v5, nextNeighbor, e1.v5, cell, e2.v5, neighbor);
             }
         } // TriangulateConnection
 
@@ -89,60 +83,39 @@ namespace Arkayns.Reckon.HM {
 
             if (leftEdgeType == HexEdgeType.Slope) {
                 if (rightEdgeType == HexEdgeType.Slope) {
-                    TriangulateCornerTerraces(
-                        bottom, bottomCell, left, leftCell, right, rightCell
-                    );
+                    TriangulateCornerTerraces(bottom, bottomCell, left, leftCell, right, rightCell);
+                } else if (rightEdgeType == HexEdgeType.Flat) {
+                    TriangulateCornerTerraces(left, leftCell, right, rightCell, bottom, bottomCell);
+                } else {
+                    TriangulateCornerTerracesCliff(bottom, bottomCell, left, leftCell, right, rightCell);
                 }
-                else if (rightEdgeType == HexEdgeType.Flat) {
-                    TriangulateCornerTerraces(
-                        left, leftCell, right, rightCell, bottom, bottomCell
-                    );
-                }
-                else {
-                    TriangulateCornerTerracesCliff(
-                        bottom, bottomCell, left, leftCell, right, rightCell
-                    );
-                }
-            }
-            else if (rightEdgeType == HexEdgeType.Slope) {
+            } else if (rightEdgeType == HexEdgeType.Slope) {
                 if (leftEdgeType == HexEdgeType.Flat) {
-                    TriangulateCornerTerraces(
-                        right, rightCell, bottom, bottomCell, left, leftCell
-                    );
+                    TriangulateCornerTerraces(right, rightCell, bottom, bottomCell, left, leftCell);
+                } else {
+                    TriangulateCornerCliffTerraces(bottom, bottomCell, left, leftCell, right, rightCell);
                 }
-                else {
-                    TriangulateCornerCliffTerraces(
-                        bottom, bottomCell, left, leftCell, right, rightCell
-                    );
-                }
-            }
-            else if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
+            } else if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
                 if (leftCell.Elevation < rightCell.Elevation) {
-                    TriangulateCornerCliffTerraces(
-                        right, rightCell, bottom, bottomCell, left, leftCell
-                    );
+                    TriangulateCornerCliffTerraces(right, rightCell, bottom, bottomCell, left, leftCell);
+                } else {
+                    TriangulateCornerTerracesCliff(left, leftCell, right, rightCell, bottom, bottomCell);
                 }
-                else {
-                    TriangulateCornerTerracesCliff(
-                        left, leftCell, right, rightCell, bottom, bottomCell
-                    );
-                }
-            }
-            else {
+            } else {
                 AddTriangle(bottom, left, right);
                 AddTriangleColor(bottomCell.Color, leftCell.Color, rightCell.Color);
             }
         } // TriangulateCorner
 
         private void TriangulateEdgeTerraces(EdgeVertices begin, HexCell beginCell, EdgeVertices end, HexCell endCell) {
-            EdgeVertices e2 = EdgeVertices.TerraceLerp(begin, end, 1);
-            Color c2 = HexMetrics.TerraceLerp(beginCell.Color, endCell.Color, 1);
+            var e2 = EdgeVertices.TerraceLerp(begin, end, 1);
+            var c2 = HexMetrics.TerraceLerp(beginCell.Color, endCell.Color, 1);
 
             TriangulateEdgeStrip(begin, beginCell.Color, e2, c2);
 
-            for (int i = 2; i < HexMetrics.terraceSteps; i++) {
-                EdgeVertices e1 = e2;
-                Color c1 = c2;
+            for (var i = 2; i < HexMetrics.terraceSteps; i++) {
+                var e1 = e2;
+                var c1 = c2;
                 e2 = EdgeVertices.TerraceLerp(begin, end, i);
                 c2 = HexMetrics.TerraceLerp(beginCell.Color, endCell.Color, i);
                 TriangulateEdgeStrip(e1, c1, e2, c2);
@@ -152,19 +125,19 @@ namespace Arkayns.Reckon.HM {
         } // TriangulateEdgeTerraces
 
         private void TriangulateCornerTerraces(Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell) {
-            Vector3 v3 = HexMetrics.TerraceLerp(begin, left, 1);
-            Vector3 v4 = HexMetrics.TerraceLerp(begin, right, 1);
-            Color c3 = HexMetrics.TerraceLerp(beginCell.Color, leftCell.Color, 1);
-            Color c4 = HexMetrics.TerraceLerp(beginCell.Color, rightCell.Color, 1);
+            var v3 = HexMetrics.TerraceLerp(begin, left, 1);
+            var v4 = HexMetrics.TerraceLerp(begin, right, 1);
+            var c3 = HexMetrics.TerraceLerp(beginCell.Color, leftCell.Color, 1);
+            var c4 = HexMetrics.TerraceLerp(beginCell.Color, rightCell.Color, 1);
 
             AddTriangle(begin, v3, v4);
             AddTriangleColor(beginCell.Color, c3, c4);
 
-            for (int i = 2; i < HexMetrics.terraceSteps; i++) {
-                Vector3 v1 = v3;
-                Vector3 v2 = v4;
-                Color c1 = c3;
-                Color c2 = c4;
+            for (var i = 2; i < HexMetrics.terraceSteps; i++) {
+                var v1 = v3;
+                var v2 = v4;
+                var c1 = c3;
+                var c2 = c4;
                 v3 = HexMetrics.TerraceLerp(begin, left, i);
                 v4 = HexMetrics.TerraceLerp(begin, right, i);
                 c3 = HexMetrics.TerraceLerp(beginCell.Color, leftCell.Color, i);
@@ -178,42 +151,38 @@ namespace Arkayns.Reckon.HM {
         } // TriangulateCornerTerraces
 
         private void TriangulateCornerTerracesCliff(Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell) {
-            float b = 1f / (rightCell.Elevation - beginCell.Elevation);
+            var b = 1f / (rightCell.Elevation - beginCell.Elevation);
             if (b < 0) {
                 b = -b;
             }
 
-            Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(right), b);
-            Color boundaryColor = Color.Lerp(beginCell.Color, rightCell.Color, b);
+            var boundary = Vector3.Lerp(Perturb(begin), Perturb(right), b);
+            var boundaryColor = Color.Lerp(beginCell.Color, rightCell.Color, b);
 
-            TriangulateBoundaryTriangle(
-                begin, beginCell, left, leftCell, boundary, boundaryColor
-            );
+            TriangulateBoundaryTriangle(begin, beginCell, left, leftCell, boundary, boundaryColor);
 
             if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
                 TriangulateBoundaryTriangle(left, leftCell, right, rightCell, boundary, boundaryColor);
-            }
-            else {
+            }else {
                 AddTriangleUnperturbed(Perturb(left), Perturb(right), boundary);
                 AddTriangleColor(leftCell.Color, rightCell.Color, boundaryColor);
             }
         } // TriangulateCornerTerracesCliff
 
         private void TriangulateCornerCliffTerraces(Vector3 begin, HexCell beginCell, Vector3 left, HexCell leftCell, Vector3 right, HexCell rightCell) {
-            float b = 1f / (leftCell.Elevation - beginCell.Elevation);
+            var b = 1f / (leftCell.Elevation - beginCell.Elevation);
             if (b < 0) {
                 b = -b;
             }
 
-            Vector3 boundary = Vector3.Lerp(Perturb(begin), Perturb(left), b);
-            Color boundaryColor = Color.Lerp(beginCell.Color, leftCell.Color, b);
+            var boundary = Vector3.Lerp(Perturb(begin), Perturb(left), b);
+            var boundaryColor = Color.Lerp(beginCell.Color, leftCell.Color, b);
 
             TriangulateBoundaryTriangle(right, rightCell, begin, beginCell, boundary, boundaryColor);
 
             if (leftCell.GetEdgeType(rightCell) == HexEdgeType.Slope) {
                 TriangulateBoundaryTriangle(left, leftCell, right, rightCell, boundary, boundaryColor);
-            }
-            else {
+            } else {
                 AddTriangleUnperturbed(Perturb(left), Perturb(right), boundary);
                 AddTriangleColor(leftCell.Color, rightCell.Color, boundaryColor);
             }
@@ -246,6 +215,8 @@ namespace Arkayns.Reckon.HM {
             AddTriangleColor(color);
             AddTriangle(center, edge.v3, edge.v4);
             AddTriangleColor(color);
+            AddTriangle(center, edge.v4, edge.v5);
+            AddTriangleColor(color);
         } // TriangulateEdgeFan
 
         private void TriangulateEdgeStrip(EdgeVertices e1, Color c1, EdgeVertices e2, Color c2) {
@@ -253,12 +224,14 @@ namespace Arkayns.Reckon.HM {
             AddQuadColor(c1, c2);
             AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
             AddQuadColor(c1, c2);
-            AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
+            AddQuad(e1.v3, e1.v3, e2.v3, e2.v3);
+            AddQuadColor(c1, c2);
+            AddQuad(e1.v4, e1.v5, e2.v4, e2.v5);
             AddQuadColor(c1, c2);
         } // TriangulateEdgeStrip
 
         private void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
-            int vertexIndex = m_vertices.Count;
+            var vertexIndex = m_vertices.Count;
             m_vertices.Add(Perturb(v1));
             m_vertices.Add(Perturb(v2));
             m_vertices.Add(Perturb(v3));
@@ -268,7 +241,7 @@ namespace Arkayns.Reckon.HM {
         } // AddTriangle
 
         private void AddTriangleUnperturbed(Vector3 v1, Vector3 v2, Vector3 v3) {
-            int vertexIndex = m_vertices.Count;
+            var vertexIndex = m_vertices.Count;
             m_vertices.Add(v1);
             m_vertices.Add(v2);
             m_vertices.Add(v3);
